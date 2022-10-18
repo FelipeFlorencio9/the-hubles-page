68d7300bb6121987a5a07d59b8061115ad76162d
Felipe Florencio <felipeoflorencio@hotmail.com>
2022-10-18 17:05:43 -0300

    add first vetor

---
+++ functions.js
@@ -0,0 +1,4 @@
+
+function exibeInfoHandlebar() {
+    document.getElementById("f-handlebar").innerHTML = "<img class='handlebar-vector' src='./img/Vetores/vector1.png' alt='vetor do Handlebar'><p class='handlebar-text'>Handlebar</p>";
+}
